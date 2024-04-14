6f24111fc7429178ee01e2c9cfc4357c67d71f3e
Aditya Pandey <adityacoderr@gmail.com>
2024-04-14 08:04:03 +0530

    Some bugs fixed

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ function capitalizeFirstLetter(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-var city = prompt("Enter City");
+let city = prompt("Enter City");
 let capitalizedPrompt = capitalizeFirstLetter(city);
-document.querySelector(".cityName").textContent = city
+document.querySelector(".cityName").textContent = capitalizedPrompt
 const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${capitalizedPrompt}`;
 const options = {
 	method: 'GET',
@@ -19,13 +19,19 @@ var data=async ()=>{
 	const response = await fetch(url, options);
 	const result = await response.json();
      console.log(result);
-     
-document.querySelector(".temp").textContent = result["temp"] + "°C";
-document.querySelector(".feel").textContent = result["feels_like"] + "°C";
-document.querySelector(".mintemp").textContent = result["min_temp"] + "°C";
-document.querySelector(".maxtemp").textContent = result["max_temp"] + "°C";
-document.querySelector(".humidity").textContent = result["humidity"];
-document.querySelector(".lastupdated").textContent = result["humidity"];
+     if(result["temp"] !== undefined){
+		document.querySelector(".temp").textContent = result["temp"] + "°C";
+		document.querySelector(".feel").textContent = result["feels_like"] + "°C";
+		document.querySelector(".mintemp").textContent = result["min_temp"] + "°C";
+		document.querySelector(".maxtemp").textContent = result["max_temp"] + "°C";
+		document.querySelector(".humidity").textContent = result["humidity"];
+		document.querySelector(".lastupdated").textContent = result["humidity"];
+		
+	}
+	else{
+		alert("Enter Correct Location");
+	}
+
 };
 
 data();
